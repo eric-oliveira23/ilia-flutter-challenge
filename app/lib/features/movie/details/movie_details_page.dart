f389975c4669aa62650b88a9ef1aef46766a116c
Eric Oliveira <ericpoliveira23@gmail.com>
2024-08-24 22:17:43 -0300

    refac: layout minor changes

--- app/lib/features/movie/details/movie_details_page.dart
+++ app/lib/features/movie/details/movie_details_page.dart
@@ -85,35 +85,31 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                           overflow: TextOverflow.ellipsis,
                         ),
                       ),
-                      background: Stack(
-                        children: [
-                          Align(
-                            alignment: Alignment.topCenter,
-                            child: Container(
-                              height: expandedHeight - collapsedHeight - 20,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20),
-                                ),
-                                image: DecorationImage(
-                                  colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
-                                  image: CachedNetworkImageProvider(
-                                    '$imageBaseUrl/w500/${state.data!.posterPath}',
-                                  ),
-                                  fit: BoxFit.fill,
-                                ),
+                      background: Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          height: expandedHeight - collapsedHeight - 20,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                            image: DecorationImage(
+                              colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+                              image: CachedNetworkImageProvider(
+                                '$imageBaseUrl/w500/${state.data!.posterPath}',
                               ),
+                              fit: BoxFit.fill,
                             ),
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
                   SliverToBoxAdapter(
                     child: Container(
                       child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        padding: EdgeInsets.symmetric(horizontal: 25),
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
@@ -133,10 +129,12 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                               child: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: state.data?.genres
-                                        ?.map((e) => Padding(
-                                              padding: const EdgeInsets.all(8.0),
-                                              child: Tag(child: Text(e.name)),
-                                            ))
+                                        ?.map(
+                                          (e) => Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Tag(child: Text(e.name)),
+                                          ),
+                                        )
                                         .toList() ??
                                     [],
                               ),
@@ -152,7 +150,7 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                                     Text('${state.data!.runtime.toString()} minutes'),
                                   ],
                                 ),
-                                SizedBox(width: 16),
+                                SizedBox(width: 30),
                                 Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
@@ -160,7 +158,7 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                                     Text(state.data!.spokenLanguages?.first.name ?? ''),
                                   ],
                                 ),
-                                SizedBox(width: 16),
+                                SizedBox(width: 30),
                                 Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
@@ -170,13 +168,68 @@ class _MovieDetailsPageState extends State<MovieDetailsPage> {
                                 ),
                               ],
                             ),
-                            SizedBox(height: 16),
+                            SizedBox(height: 30),
                             Text(state.data!.title, style: Theme.of(context).textTheme.headlineMedium),
                             Text(state.data!.overview, style: Theme.of(context).textTheme.bodyMedium),
-                            Text(state.data!.releaseDate, style: Theme.of(context).textTheme.bodySmall),
-                            Text(state.data!.voteAverage.toStringAsFixed(2),
-                                style: Theme.of(context).textTheme.bodySmall),
-                            SizedBox(height: 1000),
+                            Text(
+                              state.data!.voteAverage.toStringAsFixed(2),
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                            SizedBox(height: 30),
+                            Text('Production Companies', style: Theme.of(context).textTheme.titleMedium),
+                            SizedBox(
+                              height: 200,
+                              child: ListView.builder(
+                                itemCount: state.data!.productionCompanies!.length,
+                                scrollDirection: Axis.horizontal,
+                                itemBuilder: (context, i) {
+                                  final company = state.data!.productionCompanies![i];
+                                  return Tooltip(
+                                    message: company.name,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16),
+                                      child: Container(
+                                        width: 130,
+                                        height: 130,
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey.shade200,
+                                          borderRadius: BorderRadius.circular(30),
+                                        ),
+                                        padding: const EdgeInsets.all(8),
+                                        child: Column(
+                                          mainAxisAlignment: MainAxisAlignment.center,
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: CachedNetworkImage(
+                                                height: 80,
+                                                width: 80,
+                                                imageUrl: '$imageBaseUrl/w200/${company.logoPath ?? ""}',
+                                                errorWidget: (context, url, error) => const Icon(
+                                                  Icons.image_not_supported_outlined,
+                                                  size: 50,
+                                                ),
+                                                fit: BoxFit.contain,
+                                              ),
+                                            ),
+                                            Text(
+                                              company.name,
+                                              style: Theme.of(context).textTheme.bodyMedium,
+                                              maxLines: 2,
+                                              overflow: TextOverflow.ellipsis,
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            Text('Videos', style: Theme.of(context).textTheme.titleMedium),
+                            SizedBox(height: 16),
                           ],
                         ),
                       ),
--- design_system/lib/widgets/common/tag.dart
+++ design_system/lib/widgets/common/tag.dart
@@ -11,7 +11,7 @@ class Tag extends StatelessWidget {
         borderRadius: BorderRadius.circular(30),
         color: Colors.grey.shade200,
       ),
-      padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+      padding: const EdgeInsets.all(8),
       child: child,
     );
   }
